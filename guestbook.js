77f575bd5529d33fffcd486cf47e49170610f752
Erfan Bashar <erfanbashar@users.noreply.github.com>
2023-03-31 23:54:40 +0600

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,16 @@
 /**
  * Web application
  */
-const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/6dcd0e48-26a4-46e3-bd76-6a6616c6e8e9/guestbook';
+//const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/6dcd0e48-26a4-46e3-bd76-6a6616c6e8e9/guestbook';
+const apiUrlGet = 'https://us-south.functions.appdomain.cloud/api/v1/web/6dcd0e48-26a4-46e3-bd76-6a6616c6e8e9/guestbook/read-guestbook-entries-sequence';
+const apiUrlPost = 'https://us-south.functions.appdomain.cloud/api/v1/web/6dcd0e48-26a4-46e3-bd76-6a6616c6e8e9/guestbook/save-guestbook-entry-sequence';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: '${apiUrl}/read-guestbook-entries-sequence',
+      //url: '${apiUrl}/read-guestbook-entries-sequence',
+      url: '${apiUrlGet}',
       dataType: 'json'
     });
   },
@@ -16,7 +19,8 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: '${apiUrl}/save-guestbook-entry-sequence',
+      //url: '${apiUrl}/save-guestbook-entry-sequence',
+      url: '${apiUrlPost}',
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
